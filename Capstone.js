476e5f6fff0f598de96f34f06e38083b2f24dbd6
chingyutseng0715 <168664925+chingyutseng0715@users.noreply.github.com>
2024-05-02 14:50:40 -0700

    v3

--- Capstone.js
+++ Capstone.js
@@ -4,8 +4,22 @@ window.onload = function() {
 function changeContent(contentType) {
     var contentArea = document.getElementById("contentArea");
     switch (contentType) {
-        case 'Basic informations':
-            contentArea.innerHTML = "<p>This is preparation content.</p>";
+        case 'Basic information':
+            contentArea.innerHTML = contentArea.innerHTML = `
+            <div class="BI"><h2>Basic information</h2></div>
+            <ul>
+                <li>Name: Ching-yu Tseng/Eugene/曾井昱</li>
+                <li>Things I love: Family, friends, piano, Japanese culture and programming</li>
+                <li>My most cherished experience: the chance of coming to Canada and visiting Switzerland last year</li>
+                <li>My upbringing: Born in Taiwan, and come to Canada at 12th grade</li>
+                <li>My beliefs: Blieve that I can change my fate by working hard</li>
+                <li>My strengths: Math, programming, piano</li>
+                <li>My challenges: Applying for university, finishing Capstone, etc.</li>
+            </ul>
+            <br><br>
+            <img src="Parachute.jpg" alt="Parachute" width="550" height="350">
+        `;
+        break;
             break;
         case 'Hobbies and Personalities':
             contentArea.innerHTML = "<p>This is courses content.</p>";
@@ -34,8 +48,9 @@ function changeContent(contentType) {
         case 'World':
             contentArea.innerHTML = "<p>World</p>";
             break;
-
-        
+        case 'Core':
+            contentArea.innerHTML = "<p>Core Competency reflections</p>";
+            break;
 
         }
 }
